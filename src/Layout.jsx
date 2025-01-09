1c9311c70c2e0b8b842c65c25f3a1564a9d6bd6e
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2025-01-09 15:06:21 +0530

    React Query and Github Dynamically Fetch Repos

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,19 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import { Outlet } from "react-router-dom"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 function Layout() {
   return (
     <>
-     <Header/>
-     <Outlet/>
-     <Footer/>
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
